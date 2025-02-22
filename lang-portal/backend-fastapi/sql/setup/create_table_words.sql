930ef988f0925ef3f47feb7ae87f1a06052ebac9
Sadaira <sadaira.packer@gmail.com>
2025-02-21 22:16:31 -0500

    modified words table for spanish

--- lang-portal/backend-fastapi/sql/setup/create_table_words.sql
+++ lang-portal/backend-fastapi/sql/setup/create_table_words.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS words (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  kanji TEXT NOT NULL,
-  romaji TEXT NOT NULL,
+  spanish TEXT NOT NULL,
   english TEXT NOT NULL,
   parts TEXT NOT NULL  -- Store parts as JSON string
 );
